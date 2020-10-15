5dbe7e9712f6fae407dc4fa9f527fbf54e8c509d
José Alberto Rodríguez Godoy <josealberto12345@hotmail.es>
2020-10-15 12:25:47 +0200

    Creación del ejercicio 5
    Se crea programa al ejercicio 5 (contraste) de forma completa.
    
    Falta probar y ejecutar.

--- practica2/Imagen/src/pruebaImagen.cpp
+++ practica2/Imagen/src/pruebaImagen.cpp
@@ -19,6 +19,7 @@ int main (int argc, char* argv[]){
 	Imagen imagen3 = ej3_zoom((char *) "imagenes/vacas.pgm", (char *) "ej3.pgm", 100, 100, 200, 200);
 
 	//Imagen imagen4 = ej4_icono((char *) "imagenes/vacas.pgm", (char *) "ej4.pgm", 84, 125);
+	Imagen imagen5 = ej5_contrate((char*) "imagenes/celulas.pgm", (char*) "ej5.pgm", 3, 250);
 
 	return 0;
 };
@@ -56,3 +57,42 @@ Imagen ej4_icono(char* fich_orig, char* fich_rdo, const int nf, const int nc){
 	
 }
 */
+
+Imagen ej5_contrate(char* fichE, char* fichS, const int min, const int max){
+	// Fórmula a aplicar a cada píxel -> t(z) = z' = min + [((max-min) / (b-a)) * (z-a)];
+	// min, max = Niveles límite de la nueva imagen de salida.
+	// z = pixel actual.
+	// Parte constante (calcular de forma independiente una única vez) = [(max-min) / (b-a)]
+
+	// Construir una imagen desde fichero:
+	Imagen imagen;
+	imagen.LeerImagen(fichE);
+
+	// Obtener los valores límite de la imagen de entrada (Podemos hacer una función independiente):
+	int a = 256;
+	int b = 0;
+	for(int i=0; i<imagen.num_filas(); i++){
+		for(int j=0; j<imagen.num_columnas(); i++){	// Necesario poner dos if ya que se pueden cumplir las dos condiciones.
+			if(imagen.valor_pixel(i,j)<a){
+				a = imagen.valor_pixel(i,j);
+			}
+			if(imagen.valor_pixel(i,j)>b){	
+				b = imagen.valor_pixel(i,j);
+			}
+		}
+	}
+
+	// Definir la parte constante:
+	int constante = ((max-min)/(b-a));
+
+	// Contrastando la imagen:
+	for(int i=0; i<imagen.num_filas(); i++){
+		for(int j=0; j<imagen.num_columnas(); j++){
+			imagen.asigna_pixel(i,j,(min+(constante*(imagen.valor_pixel(i,j)-a))));
+		}
+	}
+
+	// Guardar el resultado en un fichero:
+	imagen.EscribirImagen(fichS);
+	return imagen;
+}
