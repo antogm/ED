d895e30bbd4b092035b53d631e97734b99b95bc2
antogm <54458569+antogm@users.noreply.github.com>
2020-12-13 14:44:31 +0100

    p5

---
+++ practica5/include/Paises.h
@@ -0,0 +1,95 @@
+#ifndef paises_h
+#define paises_h
+
+#include "Pais.h"
+#include "Punto.h"
+#include <set>
+using namespace std;
+
+class Paises{
+  private:
+    set<Pais> datos;
+  public:
+      Paises(){}
+      void Insertar(const Pais &P){
+	  ................
+      }
+      void Borrar(const Pais &P){
+	  ................
+      }
+      
+      class const_iterator;
+	class iterator{
+	private:
+	    set<Pais>::iterator p;
+	public:
+	    ............
+           ..............
+	  .............
+.	    .........    
+	    friend class Paises;
+	    friend class const_iterator;
+	};    
+	class const_iterator{
+	private:
+	    set<Pais>::const_iterator p;
+	public:
+	    ..........
+            ...........
+	    friend class Paises;
+	   
+	};
+	iterator begin(){
+	  iterator it;
+	  it.p = datos.begin();
+	  return it;
+	}  
+	
+	const_iterator begin()const{
+	  ...........
+	}
+	iterator end(){
+	  iterator it;
+	  it.p = datos.end();
+	  return it;
+	}
+	const_iterator end()const{
+	  ...........
+	}
+	iterator find(const Pais &p){
+	    iterator it;
+	    set<Pais>::iterator i;
+	    for (i=datos.begin(); i!=datos.end() && !((*i)==p);++i);
+	    it.p=i;
+	    return it;
+	}
+	iterator find(const Punto &p){
+	    .............
+	}
+	friend istream & operator>>(istream & is, Paises & R){
+	      Paises rlocal;
+	      //leemos el comentario
+	      if (is.peek()=='#'){
+		string a;
+		getline(is,a);
+	      }	
+	      
+	      Pais P;
+	      while (is>>P){
+		  rlocal.Insertar(P);
+		  
+	      }
+	      R=rlocal;
+	      return is;
+	}
+	friend ostream & operator<<(ostream & os, const Paises &R){
+	  
+	    Paises::const_iterator it;
+	    for (it=R.begin(); it!=R.end(); ++it){
+		os<<*it<<"\t";
+	    }
+	    return os;
+	}
+};
+
+#endif
